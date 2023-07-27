9b3356d3d35d31bd4a15fedaa5c75ac0dd1e131f
Amanda Tuyen <atuyen26@gmail.com>
2023-07-27 15:42:30 -0700

    adds in rough contact form

--- app/page.jsx
+++ app/page.jsx
@@ -1,27 +1,31 @@
 // import "@styles/globals.css";
+import ContactForm from "@components/ContactForm";
 import Image from "next/image";
+import Link from "next/link";
 
 const Home = () => {
   return (
     <>
       <Image
-        src="https://user-images.githubusercontent.com/77664153/256369214-0cb55a1b-4308-4679-b523-dadde14eaa09.png"
+        src="https://user-images.githubusercontent.com/77664153/256354157-2c9f4248-5e41-48db-85c6-670654317e1d.jpeg"
         width={0}
         height={0}
-        sizes="50vw"
-        style={{ width: "100vw", height: "auto"}}
+        sizes="100vw"
+        style={{ width: "100vw", height: "auto" }}
         className="z[0]"
       />
-
-     {/* <div className="absolute top-0 left-0 right-0 bottom-0 h-[auto] bg-black/30 z-[1]" /> */}
       <div className="flex flex-col items-center text-white z-[1]">
         <h1 className="absolute text-white font-boldHeading text-6xl tracking-wider top-[40%] text-center z-[10] px-4">
-         Precision CNC services
+          Precision CNC services
         </h1>
-        <p className="absolute justify-center w-[80%] md:w-[70%] py-2 md:py-10 top-[60%] sm:top-[55%] font-heading text-2xl md:text-3xl text-white text-center font-thin z-[10]">
-          Website Currently Under Construction
+        <p className="absolute w-[80%] md:w-[70%] py-2 md:py-6 top-[50%] sm:top-[50%] font-heading text-2xl md:text-3xl text-white text-center font-light z-[10]">
+          Locally based in Burnaby, BC
         </p>
+        <button className="absolute text-white font-boldHeading text-6xl tracking-wider top-[70%] text-center z-[10] px-4 py-3 border-2 rounded-lg">
+          <Link href={"/"}>Contact US</Link>
+        </button>
       </div>
+      <ContactForm />
     </>
   );
 };
---
+++ components/ContactForm.jsx
@@ -0,0 +1,37 @@
+
+const ContactForm = () => {
+    return (
+        <div className="flex justify-center items-center text-white bg-black">
+          <div className='flex flex-col justify-center'>
+            <h1 className="font-heading text-3xl">Contact Us</h1>
+            <p>Please fill out the form with your project details and we will be in touch</p>
+            <form name="Quote-Request" method="post">
+              <input type="hidden" name="form-name" value="Maxum-Contact" />
+              <div>
+                <label htmlFor="name">Name:</label>
+                <input type="text" name="name" required />
+              </div>
+              <div>
+                <label htmlFor="company">Company:</label>
+                <input type="text" name="company" />
+              </div>
+              <div>
+                <label htmlFor="phone">Phone #:</label>
+                <input type="tel" name="phone" required />
+              </div>
+              <div>
+                <label htmlFor="email">Email:</label>
+                <input type="email" name="email" required />
+              </div>
+              <div>
+                <label htmlFor="project-details">Project Details:</label>
+                <textarea name="project-details" required />
+              </div>
+              <button type="submit">Send</button>
+            </form>
+          </div>
+        </div>
+      );
+}
+
+export default ContactForm
