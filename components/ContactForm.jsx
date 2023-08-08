41c715e068413974763559ab3717f25135c9fb6d
Amanda Tuyen <atuyen26@gmail.com>
2023-08-08 14:54:40 -0700

    changes icons and some styling of contacts

--- components/ContactForm.jsx
+++ components/ContactForm.jsx
@@ -2,54 +2,57 @@ import Image from "next/image";
 
 const ContactForm = () => {
   return (
-    <div className="flex flex-col items-center">
-      <h1 className="flex justify-center pt-16 bg-slate-400 w-full ">Contact Us</h1>
-      
-      <div className="bg-slate-400 flex justify-evenly w-full py-12">
-        <div className="text-white text-center">
+    <div className="flex flex-col items-center bg-red-500">
+      <h1 className="flex justify-center pt-16 bg-slate-400 w-full text-white">
+        Contact Us
+      </h1>
+
+      <div className="bg-slate-400 grid grid-cols-3 w-full py-12">
+        <div className="flex flex-col items-center text-white text-center">
           <div className="bg-blue-500 rounded-full p-10">
             <Image
-              src="https://user-images.githubusercontent.com/77664153/189387060-cabc9d81-b64d-466d-bf56-d5233f03e7fa.png"
+              // src="https://user-images.githubusercontent.com/77664153/189387060-cabc9d81-b64d-466d-bf56-d5233f03e7fa.png"
+              src="https://user-images.githubusercontent.com/77664153/259216521-a242cf3a-72be-4c76-b70d-a39967a2947c.png"
               width={100}
               height={100}
               alt="map icon"
             />
           </div>
-          <h1>Address</h1>
-          <p>7220 Winston St.</p>
-          <p>Burnaby, BC V5A 2G9</p>
+          <h1 className="my-8">Address</h1>
+          <p className="text-lg">7220 Winston St.</p>
+          <p className="text-lg">Burnaby, BC V5A 2G9</p>
         </div>
 
-        <div className="text-white text-center">
+        <div className="flex flex-col items-center text-white text-center">
           <div className="bg-blue-500 rounded-full p-10">
             <Image
-              src="https://user-images.githubusercontent.com/77664153/189203263-d8200e4d-2aa4-4b03-b138-bc12c7e758f9.png"
-              width={100}
+              src="https://user-images.githubusercontent.com/77664153/259215682-e6ed4e45-a152-44cf-bfe8-948345475e16.png"
+              // src="https://user-images.githubusercontent.com/77664153/259213393-694cc4b3-b608-4b68-be4e-a50c0daf4a87.png"
+              // src="https://user-images.githubusercontent.com/77664153/189203263-d8200e4d-2aa4-4b03-b138-bc12c7e758f9.png"
+              width={90}
               height={100}
               alt="email icon"
             />
           </div>
-          <h1 className="mt-10">Email</h1>
-          <p>info@metaldist.com</p>
-     
+          <h1 className="my-8">Email</h1>
+          <p className="text-lg">info@metaldist.com</p>
         </div>
 
-        <div className="text-white text-center">
+        <div className="flex flex-col items-center text-white text-center">
           <div className="bg-blue-500 rounded-full p-10">
             <Image
-              src="https://user-images.githubusercontent.com/77664153/189227458-6398c5cc-22ab-47ea-8149-cba5ba7d6f23.png"
+              src="https://user-images.githubusercontent.com/77664153/259216034-54a5b4b3-7e62-4670-9806-76398237d6f6.png"
+              // src="https://user-images.githubusercontent.com/77664153/189227458-6398c5cc-22ab-47ea-8149-cba5ba7d6f23.png"
               width={100}
               height={100}
               alt="phone icon"
             />
           </div>
-          <h1>Phone</h1>
-          <p>604-420-3731</p>
-          <p>1-877-420-3731 (toll-free)</p>
+          <h1 className="my-8">Phone</h1>
+          <p className="text-lg">604-420-3731</p>
+          <p className="text-lg">1-877-420-3731 (toll-free)</p>
         </div>
       </div>
-
-
 
       <div className="flex flex-row justify-start w-full">
         <div className="flex flex-col justify-center items-center text-black my-10 w-full">
@@ -99,18 +102,16 @@ const ContactForm = () => {
           </div>
         </div>
 
-        <div className="flex flex-col items-start w-full mt-10">
+        <div className="flex flex-col items-center w-full mt-10">
           <h2>Hours Of Operation</h2>
           <ul>
             <li>Monday - Thursday: 8am - 4pm</li>
             <li>Friday: 8am - 3pm </li>
             <li>Saturday - Sunday: Closed</li>
-            <li></li>
-            <li></li>
+            <li>Stat Holidays: Closed</li>
           </ul>
         </div>
       </div>
-
     </div>
   );
 };
