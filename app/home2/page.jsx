71ef6410d069b36b1636c045493f9c41cce19adc
Amanda <atuyen26@gmail.com>
2024-03-12 11:38:02 -0700

    add in dummy home2 page for editting

---
+++ app/home2/page.jsx
@@ -0,0 +1,12 @@
+import ContactForm from "@components/ContactForm";
+import Hero from "@components/Hero";
+
+const Home2 = () => {
+  return (
+    <>
+      <Hero />
+    </>
+  );
+};
+
+export default Home2;
