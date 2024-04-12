77009eaaec76441f80b7728f63d1a6c6bce46567
Amanda <atuyen26@gmail.com>
2024-04-12 09:19:57 -0700

    adds video to home pg

--- app/page.jsx
+++ app/page.jsx
@@ -8,10 +8,19 @@ const Home = () => {
     <>
       <Hero
         heading={"PRECISION CNC MACHINING"}
-        message={"Specializing in Lead (Pb), Zinc (Zn), Aluminum (Al) and Steel"}
-        img={"/images/maxum-home.png"}
+        message={
+          "Specializing in Lead (Pb), Zinc (Zn), Aluminum (Al) and Steel"
+        }
+        img={""}
       />
 
+      <video
+        src={require("../public/videos/maxum-video-bg.mp4")}
+        autoPlay
+        muted
+        loop
+        className="absolute z[0] top-0 left-0 object-fill h-screen w-screen mb-0"
+      />
     </>
   );
 };
