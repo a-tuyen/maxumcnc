6b2527620e50471608d9235db1f2a79a54bc8afe
Amanda <atuyen26@gmail.com>
2024-04-19 15:46:11 -0700

    starts adding in home text

--- app/home2/page.jsx
+++ app/home2/page.jsx
@@ -28,20 +28,11 @@ const Home2 = () => {
       </button> */}
 
       <div className="flex flex-col md:flex-row justify-center items-center bg-accent1 px-1 md:py-4 ">
-        {/* <h1 className="font-banner text-4xl font-light mt-8 md:mt-0 text-center text-white">
-          Celebrating
-        </h1>
-        <h1 className="font-heading text-4xl bold font-medium text-accent2 my-4 md:my-0 md:px-3">
-          75 years
-        </h1>
-        <h1 className="font-banner text-4xl font-light mb-8 md:mb-0 text-center text-white">
-          in the Metal Fabrication Industry
-        </h1> */}
         <div className="flex flex-col items-center justify center lg:flex-row h-[8.5rem] lg:h-[3.5rem]">
         <h1 className="flex items-center font-heading text-4xl bold font-medium text-accent2 my-4 md:my-0 md:px-4">
           Celebrating 75 years of
         </h1>
-        <div className="overflow-hidden h-[3.25rem] mt-4 lg:mt-0">
+        <div className="overflow-hidden h-[3.25rem] mt-2 md:mt-4 lg:mt-0">
         <ul className="animate-titleflip text-center lg:text-left">
           <li className="font-serif bold italic text-white text-5xl">
           Precision 
@@ -61,8 +52,14 @@ const Home2 = () => {
         </ul>
         </div>
         </div>
-
       </div>
+
+      <article className="bg-accent1 text-white tracking-wide w-screen flex justify-center items-center text-center">
+        <p className="w-[90%] lg:w-[60%] my-2 md:my-0"> Maxum CNC is a brand of Metal Distributors and we are proud be celebrating our 75th year in the Metal Fabrication Industry! Our skilled team of fabricators, die casters and machinists have 
+</p>
+        
+
+      </article>
       <div>
         <div>
           Maxum CNC is a brand of Metal Distributors Ltd and is a trusted
